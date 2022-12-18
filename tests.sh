dcfd2858baf8e0fdb3521ef17c61338122a4e958
Hannes Bredberg <hannesbredberg@gmail.com>
2022-12-18 18:50:48 +0100

    Tests: Show compiler output on test compilation failure

--- tests.sh
+++ tests.sh
@@ -1,1 +1,1 @@
-find tests -name main.n | xargs -rI '{}' 'sh' -c 'echo -n "{}: " && zig-out/bin/bootstrap {} >/dev/null 2>&1 || exit 1 && ./a.out || exit 1'
+find tests -name main.n | xargs -rI '{}' 'sh' -c 'echo -n "{}: " && zig-out/bin/bootstrap {} >/dev/null 2>&1 || zig-out/bin/bootstrap {} || exit 1 && ./a.out || exit 1'
