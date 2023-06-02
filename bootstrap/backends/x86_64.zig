89978e32f47a03cbecc67347fd910ba5774e5fe3
Hannes Bredberg <hannesbredberg@gmail.com>
2023-06-02 16:06:36 +0200

    x86_64: Implement bit_or_constant

--- bootstrap/backends/x86_64.zig
+++ bootstrap/backends/x86_64.zig
@@ -577,6 +577,14 @@ fn writeDecl(writer: *backends.Writer, decl_idx: ir.DeclIndex.Index, uf: rega.Un
                 0x80 | boolToU8(op_t != .u8),
             }, opTypeImm(op_t, imm), false);
         },
+        .bit_or_constant => |op| {
+            const op_t = decl.instr.getOperationType();
+            const imm = std.mem.asBytes(&op.rhs);
+            try mov(writer, uf, op_t, decl_idx, op.lhs, true);
+            try writeOperandReg(writer, uf, op_t, decl_idx, 1, &.{
+                0x80 | boolToU8(op_t != .u8),
+            }, opTypeImm(op_t, imm), false);
+        },
         .inplace_add_constant => |op| {
             const op_t = decl.instr.getOperationType();
             if(op.rhs == 1) {
